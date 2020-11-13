2824424191c996bc12e9d94d78079694e1bfbcaa
pjamason <pjamason@ucsd.edu>
2020-11-12 23:24:39 -0800

    add category via api

--- test/test_api_caliper_events.py
+++ test/test_api_caliper_events.py
@@ -18,9 +18,9 @@ class TestCaliperGeneration():
     def prepare_canvas(self):
 
         # Canvas API URL
-        API_URL = "https://canvas.ucsd.edu"
+        self.API_URL = "https://canvas.ucsd.edu"
         # Canvas API key
-        API_KEY = os.getenv("CANVAS_API_KEY")
+        self.API_KEY = os.getenv("CANVAS_API_KEY")
         # Initialize a new Canvas object
         self.canvas = Canvas(self.API_URL, self.API_KEY)
         yield
@@ -29,6 +29,21 @@ class TestCaliperGeneration():
     @pytest.fixture(autouse=True)
     def myfixture(self, prepare_canvas):
         print(self.canvas)
+
+        self.course = self.canvas.get_course(20774)
+        self.group_category = self.course.create_group_category("Test String")
+        self.progress = self.group_category.assign_members()
+
+    def test_add_category(self, prepare_canvas):
+
+        self.course = self.canvas.get_course(20774)
+        print("course name: " + self.course.name)
+        # get "test assignment 1"
+        self.assignment = self.course.get_assignment(219410)
+
+        # see https://github.com/ucfopen/canvasapi/blob/develop/tests/test_assignment.py
+        # 115753: testacct3
+        user_id = 115753
 
     def test_submit(self, prepare_canvas):
 
