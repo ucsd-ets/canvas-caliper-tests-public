bd528e5afd2ffb24246be7d54b15e8a9b7dfa81b
pjamason <pjamason@ucsd.edu>
2020-12-18 00:59:19 -0800

    got file submit working

--- test/test_api_caliper_events.py
+++ test/test_api_caliper_events.py
@@ -12,6 +12,8 @@ from canvasapi.assignment import (
     AssignmentOverride,
     AssignmentExtension,
 )
+from canvasapi.course import Course, CourseNickname, LatePolicy, Page
+
 from canvasapi.group import Group, GroupCategory, GroupMembership
 
 
@@ -25,17 +27,20 @@ class TestCaliperGeneration():
         # Canvas API key
         self.API_KEY = os.getenv("CANVAS_API_KEY")
         # 115753: testacct3
+        # only used in test_submit_file - TODO confirm required
         self.USER_ID = 115753
-        # test caliper events course
-        # TODO: copy same course with testacct2 as owner so
-        # pjamason can submit to it? pjamason can't submit to own course
-        self.COURSE_ID = 20774
-        self.ASSIGNMENT_ID = 192792
+        # test caliper events course (pjamason and testacct1 teachers)
+        # pjamason can't submit to own course
+        # self.COURSE_ID = 20774
+        # self.ASSIGNMENT_ID = 192792
+        # caliper events test 2 course (testacct1 teacher)
+        self.COURSE_ID = 24284
+        self.ASSIGNMENT_ID = 241934
         # Initialize a new Canvas object
         self.canvas = Canvas(self.API_URL, self.API_KEY)
         self.requester = self.canvas._Canvas__requester
         self.course = self.canvas.get_course(self.COURSE_ID)
-        self.assignment = self.course.get_assignment(self.ASSIGNMENT_ID)
+        #self.assignment = self.course.get_assignment(self.ASSIGNMENT_ID)
 
         yield
         self.canvas = False
@@ -77,58 +82,40 @@ class TestCaliperGeneration():
         self.file.close()
 
     # submission created
-    # UNDER DEV
     # https://d1raj86qipxohr.cloudfront.net/production/caliper/event-types/submission_created.json
     # see test_submit() in assignment https://github.com/ucfopen/canvasapi/blob/develop/tests/test_assignment.py
 
-    def test_submit(self, prepare_canvas):
-
-        sub_type = "online_upload"
-        sub_dict = {"submission_type": sub_type}
-
-        # TODO: copy same course with testacct2 as owner so
-        # pjamason can submit to it? pjamason can't submit to own course
-        submission = self.assignment.submit(sub_dict)
-
-        # assert(result[0])
-        assert isinstance(submission, Submission)
-        assert submission.submission_type == sub_type
-
-        #self.assertIsInstance(submission, Submission)
-        #self.assertTrue(hasattr(submission, "submission_type"))
-        #self.assertEqual(submission.submission_type, sub_type)
-
-    # submission created (file)
-    #      # https://d1raj86qipxohr.cloudfront.net/production/caliper/event-types/submission_created.json
-    # NOT WORKING
-    # NOT WORKING: could not get submission to appear
-    # TODO: api call to SAH to confirm generation
     def test_submit_file(self, prepare_canvas):
 
-        # get "test assignment 1"
-        self.assignment = self.course.get_assignment(219410)
-
-        # see https://github.com/ucfopen/canvasapi/blob/develop/tests/test_assignment.py
+        # getting error here
+        # endpoint = 'courses/24284/assignments/219410'
+        # but assignment is 241934 above.  set it here
+        self.assignment = self.course.get_assignment(241934)
 
         filename = "tmp2.png"
         try:
             with open(filename, "w+") as file:
-                response = self.assignment.upload_to_submission(
-                    file, self.USER_ID)
-
-            # self.assertTrue(response[0])
-            assert response[0] is True
-
-            #self.assertIsInstance(response[1], dict)
-            assert type(response[1]) is dict
-
-            #            self.assertIn("url", response[1])
-            assert "url" in response[1]
-
-            print(response[0])
-            print(response[1])
+                sub_type = "online_upload"
+                sub_dict = {"submission_type": sub_type}
+                submission = self.assignment.submit(sub_dict, file)
+            assert isinstance(submission, Submission)
+            assert submission.submission_type == sub_type
+            print(submission)
             print("test")
 
+        except Exception:
+            raise Exception
+
+    # IN PROGRESS
+    # https://d1raj86qipxohr.cloudfront.net/production/caliper/event-types/wiki_page_created.json
+    def test_create_wiki_page(self, prepare_canvas):
+        try:
+            # TODO DELETE PAGE before creating if it exists
+            title = "Newest Page"
+            new_page = self.course.create_page(wiki_page={"title": title})
+
+            assert isinstance(new_page, Page)
+            assert new_page.title == title
         except Exception:
             raise Exception
 
